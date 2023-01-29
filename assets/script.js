249a4b319f344c058ff10d8f8748a5aa44b26486
JudgedGeo <115055273+Judgedgeo@users.noreply.github.com>
2023-01-29 02:19:58 -0600

    worked on HTML

--- assets/script.js
+++ assets/script.js
@@ -6,13 +6,14 @@ var descrip = document.querySelector('#description')
 var temp = document.querySelector('#temp')
 var wind = document.querySelector('#wind')
 
+//API key
 
  apik = "6eef10960e7e69d9cf4cbeecaf9a3580"
 
-//kelvin to celcious. 1 Kelvin is equal to -272.15 Celsius.
+//Convert Celsius to Fahrenheit
 
 function convertion(val){
-    return (val - 273).toFixed(2)
+    return (val + 32).toFixed(1.8)
 }
 
 
@@ -20,13 +21,11 @@ function convertion(val){
 
 
 
-        fetch('api.openweathermap.org/data/2.5/forecast?q='+inputval.value+'&appid='+apik)
+        fetch('api.openweathermap.org/data/2.5/forecast?q='+inputval.value+apik)
         .then(res => res.json())
 
 
         .then(data => {
-
-
             var nameval = data['name']
             var descrip = data['weather']['0']['description']
             var tempature = data['main']['temp']
